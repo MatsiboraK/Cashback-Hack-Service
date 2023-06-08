9e50a80de00eed43ed265894fadb39ceb15ebb4f
MatsiboraK <kmacibora50@gmail.com>
2023-06-08 17:29:33 +0700

    Initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.service;
 
-    public class CashbackHackServiceTest {
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
 
         @org.junit.Test
@@ -10,17 +12,17 @@ package ru.netology.service;
             int actual = service.remain(amount);
             int expected = 500;
 
-            org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(actual, expected);
         }
 
-        @org.junit.Test
+        @Test
         public void shouldSuggestAddingOn100() {
             int amount = 900;
 
             int actual = service.remain(amount);
             int expected = 100;
 
-            org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(actual, expected);
         }
 
         @org.junit.Test
@@ -30,77 +32,77 @@ package ru.netology.service;
             int actual = service.remain(amount);
             int expected = 900;
 
-            org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(actual, expected);
         }
 
-        @org.testng.annotations.Test
+        @org.junit.Test
         public void shouldSuggestAddingOn0() {
             int amount = 0;
 
             int actual = service.remain(amount);
             int expected = 1000;
 
-            org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(actual, expected);
         }
 
-        @org.testng.annotations.Test
+        @org.junit.Test
         public void shouldSuggestAddingOn1000() {
             int amount = 1000;
 
             int actual = service.remain(amount);
             int expected = 0;
 
-            org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(actual, expected);
         }
 
-        @org.testng.annotations.Test
+        @org.junit.Test
         public void shouldSuggestAddingOn1() {
             int amount = 999;
 
             int actual = service.remain(amount);
             int expected = 1;
 
-            org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(actual, expected);
         }
 
-        @org.testng.annotations.Test
+        @org.junit.Test
         public void shouldSuggestAddingOn999() {
             int amount = 1;
 
             int actual = service.remain(amount);
             int expected = 999;
 
-            org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(actual, expected);
         }
 
-        @org.testng.annotations.Test
+        @org.junit.Test
         public void shouldSuggestAddingOn500IfAmountMoreThen1000() {
             int amount = 1500;
 
             int actual = service.remain(amount);
             int expected = 500;
 
-            org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(actual, expected);
         }
 
-        @org.testng.annotations.Test
+        @org.junit.Test
         public void shouldSuggestAddingOn100IfAmountMoreThen1000() {
             int amount = 1900;
 
             int actual = service.remain(amount);
             int expected = 100;
 
-            org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(actual, expected);
         }
 
-        @org.testng.annotations.Test
+        @org.junit.Test
         public void shouldSuggestAddingOn1000IfAmountMoreThen1000() {
             int amount = 2000;
 
             int actual = service.remain(amount);
             int expected = 1000;
 
-            org.testng.Assert.assertEquals(actual, expected);
+            org.junit.Assert.assertEquals(actual, expected);
         }
     }
 
